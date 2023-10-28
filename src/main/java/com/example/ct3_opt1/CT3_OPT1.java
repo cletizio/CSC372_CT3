f4e4a8a36269c4a16a212dfda6f414ed406bed65
Chris Letizio <cletizio@gmail.com>
2023-10-27 22:25:14 -0400

    Got the random green background working on option 3

--- src/main/java/com/example/ct3_opt1/CT3_OPT1.java
+++ src/main/java/com/example/ct3_opt1/CT3_OPT1.java
@@ -5,12 +5,13 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.BufferedWriter;
@@ -18,22 +19,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class CT3_OPT1 extends Application  {
     TextField dateTime = null;
+    Stage primaryStage = null;
+    GridPane pane = null;
+    Scene scene = null;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane pane = null;
-        Scene scene = null;
+
 
         pane = new GridPane();
         scene = new Scene(pane);
+        Canvas canvas = new Canvas(300, 300);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         primaryStage.setTitle("User Interface Example");
         MenuItem menuItem1 = new MenuItem("Display Date/Time");
         MenuItem menuItem2 = new MenuItem("Write to txt file");
-        MenuItem menuItem3 = new MenuItem("Option 3");
+        MenuItem menuItem3 = new MenuItem("Set background green");
         MenuItem menuItem4 = new MenuItem("Exit");
         dateTime = new TextField();
         dateTime.setAlignment(Pos.CENTER);
@@ -66,7 +72,7 @@ public class CT3_OPT1 extends Application  {
         menuItem3.setOnAction(new EventHandler<>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("FIXME: Change background color");
+                setRandomGreenBackground();
             }
         });
 
@@ -96,6 +102,22 @@ public class CT3_OPT1 extends Application  {
         } catch (IOException event){
             event.printStackTrace();
             System.err.println("Unable to write to file. " + event);
+        }
+    }
+
+    private void setRandomGreenBackground(){
+        Random random = new Random();
+        final int red = 0;
+        int green = random.nextInt(256);
+        final int blue = 0;
+
+        try {
+            Color randGreen = Color.rgb (red, green, blue);
+            BackgroundFill backgroundFill = new BackgroundFill(randGreen, null, null);
+            Background background = new Background(backgroundFill);
+            pane.setBackground(background);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
